28c523d1761611e279d1dacc6e0eab4868e5a4d4
Nikola Knežević <48497634+knez@users.noreply.github.com>
2020-03-17 18:50:28 +0100

    Create basic app skeleton and GUI

--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
+var generation;
+var population;
+var simSpeed;
+var rectSize;
+
 function setup()
 {
     createCanvas(640, 480);
+    generation = 0;
+    population = 0;
+    simSpeed = 10;
+    rectSize = 10;
 }
 
 function draw()
@@ -8,8 +17,21 @@ function draw()
     background(0);
     noStroke();
     fill('white');
+
+    drawGUI();
+
+    if ((frameCount % simSpeed) === 0) {
+        // Do stuff
+    }
+
 }
 
 function keyPressed()
 {
+}
+
+function drawGUI() {
+    textSize(16);
+    text('Generation: ' + generation, 10, height - 15);
+    text('Population: ' + population, 150, height - 15);
 }
